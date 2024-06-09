fb5707b37c9e65ae28dd8cc9412bbf26e5a559a1
Carsten Gräf <evandor@gmail.com>
2024-06-09 12:01:12 +0200

    cleanups

---
+++ pages/SidePanelOpenTabsPage.vue
@@ -0,0 +1,66 @@
+<template>
+
+  <q-page padding style="padding-top: 34px">
+
+    <div class="q-ma-none">
+      <div class="q-ma-none">
+        <div class="row q-ma-none q-pa-none">
+          <div class="col-12 q-ma-none q-pa-none q-pt-md">
+
+            <SidePanelOpenTabsListViewer />
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- place QPageSticky at end of page -->
+    <q-page-sticky expand position="top" class="darkInDarkMode brightInBrightMode">
+      <FirstToolbarHelper title="Open Tabs">
+
+        <template v-slot:iconsRight>
+
+          <SidePanelToolbarTabNavigationHelper />
+
+          <SidePanelToolbarButton
+              icon="close"
+              tooltip="Close this view"
+              @click="useUiStore().sidePanelSetActiveView(SidePanelView.MAIN)"
+          />
+        </template>
+
+      </FirstToolbarHelper>
+    </q-page-sticky>
+
+  </q-page>
+
+</template>
+
+<script lang="ts" setup>
+
+import FirstToolbarHelper from "pages/sidepanel/helper/FirstToolbarHelper.vue";
+import {SidePanelView, useUiStore} from "stores/uiStore";
+import {onMounted} from "vue";
+import Analytics from "src/core/utils/google-analytics";
+import SidePanelToolbarTabNavigationHelper from "src/opentabs/pages/SidePanelToolbarTabNavigationHelper.vue";
+import SidePanelToolbarButton from "components/buttons/SidePanelToolbarButton.vue";
+import SidePanelOpenTabsListViewer from "src/opentabs/pages/SidePanelOpenTabsListViewer.vue";
+
+onMounted(() => {
+  Analytics.firePageViewEvent('SidePanelOpenTabsPage', document.location.href);
+})
+
+// const onMessageListener = async (message: any, sender: any, sendResponse: any) => {
+//   console.log(" <<< received message", message)
+//   if (message.name === "sidepanel-switch-view") {
+//     // const tsId = message.data.changedTabsetId
+//     // await useTabsetService().reloadTabset(tsId)
+//     // console.log("tsId", tsId)
+//     // sortedSpaces.value = getSortedSpaces()
+//     // tabsetsForSpaces.value = await getTabsetsForSpaces()
+//     // randomKey.value = uid()
+//     // //console.log("tabsetsForSpace", tabsetsForSpaces.value)
+//   }
+// }
+
+</script>
