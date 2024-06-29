190c68e34312e19bc343caa24aa5144a28902e76
Carsten Gräf <evandor@gmail.com>
2024-06-29 10:08:55 +0200

    wip

--- pages/SidePanelToolbarTabNavigationHelper.vue
+++ pages/SidePanelToolbarTabNavigationHelper.vue
@@ -1,24 +1,23 @@
 <template>
   <template v-if="useFeaturesStore().hasFeature(FeatureIdent.OPEN_TABS)">
     <SidePanelToolbarButton
-        icon="chevron_left"
-        tooltip="Back to last tab"
-        :disable="useTabsStore2().chromeTabsHistoryPosition === 0"
-        :color="useTabsStore2().chromeTabsHistoryPosition === 0 ? 'grey' : 'primary'"
-        @click="NavigationService.backOneTab()"/>
+      icon="chevron_left"
+      tooltip="Back to last tab"
+      :disable="useTabsStore2().chromeTabsHistoryPosition === 0"
+      :color="useTabsStore2().chromeTabsHistoryPosition === 0 ? 'grey' : 'primary'"
+      @click="NavigationService.backOneTab()"/>
 
     <SidePanelToolbarButton
-        icon="chevron_right"
-        tooltip="Return to tab before"
-        :disable="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length-1"
-        :color="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length-1 ? 'grey' : 'primary'"
-        @click="NavigationService.forwardOneTab()"/>
+      icon="chevron_right"
+      tooltip="Return to tab before"
+      :disable="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length-1"
+      :color="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length-1 ? 'grey' : 'primary'"
+      @click="NavigationService.forwardOneTab()"/>
 
     <span class="q-ma-none q-pa-none q-mx-sm text-grey-5">|</span>
   </template>
 </template>
 <script setup lang="ts">
-import {usePermissionsStore} from "stores/permissionsStore";
 import {FeatureIdent} from "src/models/FeatureIdent";
 import NavigationService from "src/services/NavigationService";
 import SidePanelToolbarButton from "src/core/components/SidePanelToolbarButton.vue";
