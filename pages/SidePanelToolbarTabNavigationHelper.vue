54d24d92f7e1baaaccd8758bbd66add184529b74
Carsten Gräf <evandor@gmail.com>
2025-02-17 08:25:06 +0100

    started actions

--- pages/SidePanelToolbarTabNavigationHelper.vue
+++ pages/SidePanelToolbarTabNavigationHelper.vue
@@ -1,27 +1,23 @@
 <template>
-  <template v-if="useFeaturesStore().hasFeature(FeatureIdent.OPEN_TABS)">
-    <SidePanelToolbarButton
-      icon="chevron_left"
-      tooltip="Back to last tab"
-      :disable="useTabsStore2().chromeTabsHistoryPosition === 0"
-      :color="useTabsStore2().chromeTabsHistoryPosition === 0 ? 'grey' : 'primary'"
-      @click.stop="NavigationService.backOneTab()" />
+  <SidePanelToolbarButton
+    icon="chevron_left"
+    tooltip="Back to last tab"
+    :disable="useTabsStore2().chromeTabsHistoryPosition === 0"
+    :color="useTabsStore2().chromeTabsHistoryPosition === 0 ? 'grey' : 'primary'"
+    @click.stop="NavigationService.backOneTab()" />
 
-    <SidePanelToolbarButton
-      class="q-mr-sm"
-      icon="chevron_right"
-      tooltip="Return to tab before"
-      :disable="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length - 1"
-      :color="
-        useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length - 1 ? 'grey' : 'primary'
-      "
-      @click.stop="NavigationService.forwardOneTab()" />
-  </template>
+  <SidePanelToolbarButton
+    class="q-mr-sm"
+    icon="chevron_right"
+    tooltip="Return to tab before"
+    :disable="useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length - 1"
+    :color="
+      useTabsStore2().chromeTabsHistoryPosition === useTabsStore2().chromeTabsHistory.length - 1 ? 'grey' : 'primary'
+    "
+    @click.stop="NavigationService.forwardOneTab()" />
 </template>
 <script setup lang="ts">
-import { FeatureIdent } from 'src/app/models/FeatureIdent'
 import SidePanelToolbarButton from 'src/core/components/SidePanelToolbarButton.vue'
-import { useFeaturesStore } from 'src/features/stores/featuresStore'
 import NavigationService from 'src/services/NavigationService'
 import { useTabsStore2 } from 'src/tabsets/stores/tabsStore2'
 </script>
