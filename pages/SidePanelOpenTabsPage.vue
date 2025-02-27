911451ccf34c59154acdcb7ccd1efbde8926f0ff
Carsten Gräf <evandor@gmail.com>
2025-02-27 19:38:57 +0100

    approaching new release

--- pages/SidePanelOpenTabsPage.vue
+++ pages/SidePanelOpenTabsPage.vue
@@ -14,11 +14,13 @@
     <!-- place QPageSticky at end of page -->
     <q-page-sticky expand position="top" class="darkInDarkMode brightInBrightMode">
       <ViewToolbarHelper title="Open Tabs" />
+      <SearchToolbarHelper placeholder="Search in all tabsets..." />
     </q-page-sticky>
   </q-page>
 </template>
 
 <script lang="ts" setup>
+import SearchToolbarHelper from 'pages/sidepanel/helper/SearchToolbarHelper.vue'
 import Analytics from 'src/core/utils/google-analytics'
 import SidePanelOpenTabsListViewer from 'src/opentabs/pages/SidePanelOpenTabsListViewer.vue'
 import ViewToolbarHelper from 'src/pages/sidepanel/helper/ViewToolbarHelper.vue'
